b28fff097ec3d28ed1081bc32c456fadc4209e22
Joe Pea <joe@trusktr.io>
2020-11-27 22:01:13 -0800

    WIP convert a piece of UI to a custom element made with @lume/element

--- src/UI_library/components/draggable_window.ts
+++ src/UI_library/components/draggable_window.ts
@@ -1,83 +1,147 @@
+import { doUntilMouseUp } from "../events/until_mouseup.js";
+import { rgbaColorContrast } from "../functions/colorContrast.js";
+import { UI_clamp } from "../functions/ui_clamp.js";
+import { Element, reactive, element } from "@lume/element";
+import { html } from "@lume/element/dist/html";
 
+@element("draggable-window")
+export class DraggableWindow extends Element {
+    @reactive text: string = "";
+    @reactive clickCallback: (box: HTMLElement) => void = () => {};
+    @reactive closeCallback: (box: HTMLElement) => void = () => {};
+    @reactive color: string = "";
+    @reactive barContent?: HTMLElement;
 
+    box?: HTMLDivElement;
+    bar?: HTMLDivElement;
 
-
-
-
-
-import { doUntilMouseUp } from "../events/until_mouseup.js"
-import { rgbaColorContrast } from "../functions/colorContrast.js"
-import { UI_clamp } from "../functions/ui_clamp.js"
-
-
-
-
-
-type DraggableWindowOptions = {
-    text : string
-    clickCallback : (box : HTMLElement) => void
-    closeCallback : (box : HTMLElement) => void
-    color : string
-    barContent? : HTMLElement
+    get root() {
+        return this;
     }
 
-export function createDraggableWindow(
-    { text, clickCallback, closeCallback, color, barContent } : DraggableWindowOptions) {
+    template = () => {
+        return html`
+            <div
+                class="window show"
+                ref=${(el: HTMLDivElement) => (this.box = el)}
+                onmousedown=${() => this.clickCallback(this.box!)}
+            >
+                <div
+                    class="draggable-window-bar"
+                    ref=${(el: HTMLDivElement) => (this.bar = el)}
+                >
+                    ${() => this.text}
+                    <button class="exit-button" onclick=${this.closeBox}>
+                        x
+                    </button>
+                    ${this.barContent || ""}
+                </div>
+            </div>
+        `;
+    };
 
-    const box = E('div', { className: 'window show' })
+    css = /* css */ `
+        /* .window {
+            left: 50vw;
+            top: 50vh;
+        } */
 
-    box.style.left = '50vw'
-    box.style.top = '50vh'
+        .draggable-window-bar {
+            backgroundColor: ${this.color || "#555"};
+            color: ${rgbaColorContrast(this.color || "#555")};
+        }
+    `;
 
-    const bar = E('div', { text: text + ' ', className: 'draggable-window-bar' })
-    const exitBtn = E('button', { text: 'x', className: 'exit-button' })
+    closeBox = () => this.closeCallback(this.box!);
 
-    bar.style.backgroundColor = color || '#555'
-    bar.style.color = rgbaColorContrast(color || '#555')
+    connectedCallback() {
+        super.connectedCallback();
 
-    box.appendChild(bar)
+        addDragFunction(this.bar!, this.box!, this.clickCallback);
+    }
 
-    if (barContent) bar.append(barContent)
-
-    bar.appendChild(exitBtn)
-
-    const closeBox = () => closeCallback(box)
-    exitBtn.onclick = closeBox
-
-    addDragFunction(bar, box, clickCallback)
-    
-    // Let the window be displayed if someone clicks on it.
-    box.onmousedown = clickCallback.bind(null, box)
-
-    box.appendChild(E('div')) // content box
-
-    return box
+    set(opts: Partial<DraggableWindowOptions>): this {
+        Object.assign(this, opts);
+        return this;
+    }
 }
 
-function addDragFunction(bar : HTMLElement, box : HTMLElement, clickCallback : Function) {
-    
-    const state = { coords: [0] }
+type DraggableWindowOptions = {
+    text: string;
+    clickCallback: (box: HTMLElement) => void;
+    closeCallback: (box: HTMLElement) => void;
+    color: string;
+    barContent?: HTMLElement;
+};
 
-    bar.onmousedown = doUntilMouseUp(mousemove, { mousedown })
+export function createDraggableWindow(opts: DraggableWindowOptions) {
+    return new DraggableWindow().set(opts);
+}
 
-    function mousedown(e : MouseEvent) {
-        state.coords = 
-            [ e.clientX 
-            , e.clientY
-            , box.offsetLeft + box.offsetWidth/2 
-            , box.offsetTop + box.offsetHeight/2
-            ]
+// export function createDraggableWindow({
+//     text,
+//     clickCallback,
+//     closeCallback,
+//     color,
+//     barContent,
+// }: DraggableWindowOptions) {
+//     const box = E("div", { className: "window show" });
 
-        clickCallback(box)
+//     box.style.left = "50vw";
+//     box.style.top = "50vh";
+
+//     const bar = E("div", {
+//         text: text + " ",
+//         className: "draggable-window-bar",
+//     });
+//     const exitBtn = E("button", { text: "x", className: "exit-button" });
+
+//     bar.style.backgroundColor = color || "#555";
+//     bar.style.color = rgbaColorContrast(color || "#555");
+
+//     box.appendChild(bar);
+
+//     if (barContent) bar.append(barContent);
+
+//     bar.appendChild(exitBtn);
+
+//     const closeBox = () => closeCallback(box);
+//     exitBtn.onclick = closeBox;
+
+//     addDragFunction(bar, box, clickCallback);
+
+//     // Let the window be displayed if someone clicks on it.
+//     box.onmousedown = clickCallback.bind(null, box);
+
+//     box.appendChild(E("div")); // content box
+
+//     return box;
+// }
+
+function addDragFunction(
+    bar: HTMLElement,
+    box: HTMLElement,
+    clickCallback: Function
+) {
+    const state = { coords: [0] };
+
+    bar.onmousedown = doUntilMouseUp(mousemove, { mousedown });
+
+    function mousedown(e: MouseEvent) {
+        state.coords = [
+            e.clientX,
+            e.clientY,
+            box.offsetLeft + box.offsetWidth / 2,
+            box.offsetTop + box.offsetHeight / 2,
+        ];
+
+        clickCallback(box);
     }
 
-    function mousemove(e : MouseEvent) {
-        const [x, y, bx, by] = state.coords
-        UI_clamp(
-            e.clientX + bx - x,
-            e.clientY + by - y,
-            box,
-            document.body,
-            { disableClamp: 2 })
+    function mousemove(e: MouseEvent) {
+        const [x, y, bx, by] = state.coords;
+        UI_clamp(e.clientX + bx - x, e.clientY + by - y, box, document.body, {
+            disableClamp: 2,
+        });
     }
 }
