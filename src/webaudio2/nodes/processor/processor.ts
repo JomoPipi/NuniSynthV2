91e98965acf7a7180c23431cc1b7847fd3cf7f04
unknown <ronaldmcorona@gmail.com>
2021-04-12 19:59:44 -0700

    cleaned up file system

--- src/webaudio2/nodes/processor/processor.ts
+++ src/webaudio2/nodes/processor/processor.ts
@@ -264,7 +264,7 @@ class CustomAudioNode extends AudioWorkletNode {
 }
 
 // const WASM = { instance: undefined }
-// WebAssembly.instantiateStreaming(fetch('song.wasm'), importObject)
+// WebAssembly.instantiateStreaming(fetch('resources/song.wasm'), importObject)
 //     .then(results => {
 //         WASM.instance = results.instance.exports
 //         console.log('wasm instance = ', WASM.instance)
@@ -402,7 +402,7 @@ export class ProcessorNode
         this.audioWorkletNode.connect(this.volumeNode)
         this.inputChannelNode.connect(this.audioWorkletNode)
         
-        const bytes = await fetch('song.wasm')
+        const bytes = await fetch('resources/song.wasm')
             .then(r => r.arrayBuffer())
 
         this.audioWorkletNode.port.postMessage({ topic: "wasm", 
